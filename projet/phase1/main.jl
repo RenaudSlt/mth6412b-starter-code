f5ce008e0fbab7eb8830092b52781e5e7c7bf9a4
edwardhannan <hannanedward@gmail.com>
2020-09-27 17:58:43 -0400

    1) data_dir contient path pour data 2) starting directory au main.jl au lieu mth6412b-starter-code

--- projet/phase1/main.jl
+++ projet/phase1/main.jl
@@ -3,20 +3,33 @@ include("edge.jl")
 include("graph.jl")
 include("read_stsp.jl")
 
-### Se placer dans le répertoire 'mth6412b-starter-code'
+### Se placer dans le répertoire 'mth6412b-starter-code/projet/phase1' ###
 
 # Choix de l'instance
 #FileName = "bayg29.tsp"   # upper row
 FileName = "gr17.tsp"     # lower diag row
 #FileName = "swiss42.tsp"  # full matrix
 
+# Sauvegarde du chemin du fichier contenant le data
+if Sys.iswindows()
+  working_directory = pwd()
+  cd("..\\..\\instances\\stsp\\")
+  data_dir = string(pwd(), "\\" , FileName)
+  cd(working_directory)  # retour au working directory
+else # Unix system
+  working_directory = pwd()
+  cd("../../instances/stsp/")
+  data_dir = string(pwd(), "/" , FileName)
+  cd(working_directory)
+end
+
 # Nom et dimension
-headers_ = read_header(string("./instances/stsp/", FileName))
+headers_ = read_header(data_dir)
 GraphName = headers_["NAME"]
 dim = parse(Int, headers_["DIMENSION"])
 
 # Création du graphe vide
-G = Graph(GraphName, Array{Node{Nothing}}(undef,0), Array{Edge{Nothing}}(undef,0))
+G = Graph(GraphName, Array{Node{Nothing}}(undef,0), Array{Edge{Nothing}}(undef,0))  
 
 # Ajout des noeuds (le champ data est égal à nothing)
 for i in 1:dim
@@ -25,7 +38,7 @@ for i in 1:dim
 end
 
 # Ajout des arêtes
-edges_, weights_ = read_edges(headers_, string("./instances/stsp/", FileName))
+edges_, weights_ = read_edges(headers_, data_dir) 
 for j in 1:length(edges_)
   local_node1 = Node(string(edges_[j][1]), nothing)
   local_node2 = Node(string(edges_[j][2]), nothing)
