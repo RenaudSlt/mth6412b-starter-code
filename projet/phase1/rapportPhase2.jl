e08bc860eab6c15d175d30d5fa76519b3124ccaf
Renaud <renaud.saltet6@gmail.com>
2020-10-02 15:49:12 -0400

    ajout du fichier rapportPhase2.jl

---
+++ projet/phase1/rapportPhase2.jl
@@ -0,0 +1,64 @@
+### A Pluto.jl notebook ###
+# v0.11.14
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 6f2bfb20-04e4-11eb-27f5-13ab53bb3434
+include("display.jl")
+
+# ╔═╡ 7140c6d0-04e3-11eb-0a12-53b3c375d2ce
+md"# MTH6412B - Rapport de la phase 2 "
+
+# ╔═╡ b32b2a90-04e3-11eb-33db-090692a410c0
+md" Edward Hallé-Hannan 1843683\
+Renaud Saltet 2035899"
+
+# ╔═╡ d37130ae-04e3-11eb-2322-fd0f422aa7d5
+md" Lien vers le dépôt de la phase 1 : [https://github.com/RenaudSlt/mth6412b-starter-code/tree/phase2/projet/phase1](https://github.com/RenaudSlt/mth6412b-starter-code/tree/phase2/projet/phase1)"
+
+# ╔═╡ d49676d0-04e3-11eb-00c6-bb5a666d47c8
+md" ***"
+
+# ╔═╡ efab60a0-04e5-11eb-3a10-413c2bfad827
+display("display.jl", 1, 6)
+
+# ╔═╡ 3679e742-04e6-11eb-1c51-51b1e1e95ccd
+md"### Structure des composantes connexes "
+
+# ╔═╡ 7c3995f0-04e6-11eb-2cb0-8f1cfbc6afe5
+
+
+# ╔═╡ 41122730-04e6-11eb-1083-a304209afddd
+md" *** "
+
+# ╔═╡ 5044d6d2-04e6-11eb-3321-23253a9f779c
+md"### Algorithme de Kruskal "
+
+# ╔═╡ 7d14dac0-04e6-11eb-12ec-735cca753dd8
+
+
+# ╔═╡ 4be32a60-04e6-11eb-3c96-b136ecd9b69a
+md" *** "
+
+# ╔═╡ 4cfadbf0-04e6-11eb-08fe-8923717ddc25
+md"### Tests unitaires et instances "
+
+# ╔═╡ 7dd78e80-04e6-11eb-2a85-61b5df06881d
+
+
+# ╔═╡ Cell order:
+# ╟─7140c6d0-04e3-11eb-0a12-53b3c375d2ce
+# ╟─b32b2a90-04e3-11eb-33db-090692a410c0
+# ╟─d37130ae-04e3-11eb-2322-fd0f422aa7d5
+# ╟─d49676d0-04e3-11eb-00c6-bb5a666d47c8
+# ╠═6f2bfb20-04e4-11eb-27f5-13ab53bb3434
+# ╠═efab60a0-04e5-11eb-3a10-413c2bfad827
+# ╟─3679e742-04e6-11eb-1c51-51b1e1e95ccd
+# ╠═7c3995f0-04e6-11eb-2cb0-8f1cfbc6afe5
+# ╟─41122730-04e6-11eb-1083-a304209afddd
+# ╟─5044d6d2-04e6-11eb-3321-23253a9f779c
+# ╠═7d14dac0-04e6-11eb-12ec-735cca753dd8
+# ╟─4be32a60-04e6-11eb-3c96-b136ecd9b69a
+# ╟─4cfadbf0-04e6-11eb-08fe-8923717ddc25
+# ╠═7dd78e80-04e6-11eb-2a85-61b5df06881d
