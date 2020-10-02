3ca4bf4f8130bf2cc2bad64d864b557a28cd3180
edwardhannan <hannanedward@gmail.com>
2020-10-02 11:08:08 -0400

    nodeTree : first version done et debut unit_test nodeTree

--- projet/phase1/unit_test_phase2.jl
+++ projet/phase1/unit_test_phase2.jl
@@ -5,4 +5,10 @@ include("kruskal_algorithm.jl")
 # Methode : same_tree
 @test ....
 
-# Mehotde : blabla
+
+# TreeNode : get_root() 
+root = TreeNode(1) # where T=Int and (parent=nothing) by default
+child_root = TreeNode(2, parent=root)
+child_of_child = TreeNode(3, parent=child_root)
+
+@test get_root(child_of_child)==root
