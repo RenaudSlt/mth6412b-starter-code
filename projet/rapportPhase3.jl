ecf175f113aa527b0f44c28cd0938169ec0de8fb
Renaud <renaud.saltet6@gmail.com>
2020-10-22 14:02:20 -0400

    création du rapport de la phase 3

---
+++ projet/rapportPhase3.jl
@@ -0,0 +1,32 @@
+### A Pluto.jl notebook ###
+# v0.11.14
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 65a04322-1490-11eb-1a4b-c37a5389d642
+include("display.jl")
+
+# ╔═╡ 15222ee0-1490-11eb-3be1-859208915b9a
+md"# MTH6412B - Rapport de la phase 3"
+
+# ╔═╡ 51c25af0-1490-11eb-0b55-5190f4edb3f7
+md" Edward Hallé-Hannan 1843683\
+Renaud Saltet 2035899"
+
+# ╔═╡ 5a8e3f50-1490-11eb-1fb1-cd877fba1796
+md" Lien vers le dépôt de la phase 3 : [https://github.com/RenaudSlt/mth6412b-starter-code/tree/phase3/projet](https://github.com/RenaudSlt/mth6412b-starter-code/tree/phase3/projet)"
+
+# ╔═╡ 61789270-1490-11eb-347f-910bb931a3c5
+md" ***"
+
+# ╔═╡ 489a4cd0-1490-11eb-2257-cf5a23f90558
+
+
+# ╔═╡ Cell order:
+# ╟─15222ee0-1490-11eb-3be1-859208915b9a
+# ╟─51c25af0-1490-11eb-0b55-5190f4edb3f7
+# ╟─5a8e3f50-1490-11eb-1fb1-cd877fba1796
+# ╟─61789270-1490-11eb-347f-910bb931a3c5
+# ╠═65a04322-1490-11eb-1a4b-c37a5389d642
+# ╠═489a4cd0-1490-11eb-2257-cf5a23f90558
