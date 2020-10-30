6d47f553e2d3c5f7b40bebc4330fbc83d234728b
edwardhannan <hannanedward@gmail.com>
2020-10-30 13:14:24 -0400

    markedNode : fin documentation

--- projet/markednode.jl
+++ projet/markednode.jl
@@ -4,9 +4,10 @@ import Base.show
 import Base.popfirst!
 import Base.isless
 
-"""Type représentant un noeud marqué avec une structure d'arbre
-    - L'attribut min_weight_ a vocation à être le poids de l'arête de poids minimal connectant le noeud au sous arbre dans l'algorithme de Prim
-    - Si parent_ == nothing => le NodeTree est une racine
+"""Type représentant un noeud marqué avec une structure d'arbre : structure utile pour prim_algorithm.jl
+    - L'attribut min_weight_  contient le poids de l'arête de poids minimal connectant le noeud au sous-arbre
+     (c-à-d au noeud dans le sous-arbre le plus près) dans l'algorithme de Prim
+    - Si parent_ == nothing => le markedNode est une racine ou un noeud isolé 
 """
 
 mutable struct MarkedNode{T} <: AbstractNode{T}
@@ -22,6 +23,8 @@ end
     - Argument facultatif : name_ et min_weight
         name_ = "", par défaut
         min_weight_ = Inf, par défaut
+    - Initilisation :
+        parent_ = nothing (algorithme de Prim)
 """
 function MarkedNode{T}(data_::T; name_::String="", min_weight_::Number=Inf) where T
     MarkedNode(name_, data_, false, max(0.0, min_weight_), nothing)
@@ -61,12 +64,21 @@ show(node::MarkedNode) = println("node $(get_name(node)) of minimum weight to tr
 isless(node1::MarkedNode{T}, node2::MarkedNode{T}) where T = isless(get_min_weight(node1), get_min_weight(node2))
 
 """ Retire et renvoie le noeud d'attribut min_weight_ minimal dans un vecteur de MarkedNodes
-  - Input : vecteur de MarkedNodes
-  - Output : noeud d'attribut min_weight_ minimal 
+    -Utilité : permet de simuler le comportement d'une PriorityQueue à partir d'un vecteur standard
+    Input : vecteur de MarkedNodes
+     Output : noeud d'attribut min_weight_ minimal 
 """
 function popfirst!(nodes::Vector{MarkedNode{T}}) where T  
+  
+  # Si le vecteur est vide, on lance directement l'erreur : permet d'éviter que l'erreur se propage à minimum, findfirst, etc. (plus difficile à déboguer) 
+  if (isempty(nodes))
+    @error("The vector is empty.") 
+  end
+
   min_node = minimum(nodes)
   idx = findfirst(x -> x == min_node, nodes)
+
+  # Attention : Modification du vecteur de nodes 
   deleteat!(nodes, idx)
-  min_node
+  return min_node
 end
