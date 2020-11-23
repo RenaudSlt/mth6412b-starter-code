02c4e6d37233852911e4a4e9a6300ffa4d479c7e
Renaud <renaud.saltet6@gmail.com>
2020-11-23 13:33:41 -0500

    rapport

--- projet/rapportPhase4.jl
+++ projet/rapportPhase4.jl
@@ -7,6 +7,9 @@ using InteractiveUtils
 # ╔═╡ 6a686500-2132-11eb-0a1a-6d0bc5b501a9
 include("display.jl")
 
+# ╔═╡ 284f3500-2da7-11eb-01bb-8b18721a7ff8
+include("display_img.jl")
+
 # ╔═╡ cc7ea210-2130-11eb-2b97-8523da844f3e
 md"# MTH6412B - Rapport de la phase 4"
 
@@ -115,7 +118,7 @@ md"On retrouve enfin les arêtes correspondant à cette tournée, on les mémori
 display("rsl_algorithm.jl",39,61)
 
 # ╔═╡ 62bdc8f0-214f-11eb-19c3-8b563e1716da
-md"#### Tests"
+md"#### Premiers tests"
 
 # ╔═╡ 67f14450-214f-11eb-3ffa-d51f5e758c23
 md" Nous avons testé l'algorithme avec toutes les instances TSP symétriques en comparant le poids de la tournée renvoyée au poids optimal indiqué sur le site web [http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/STSP.html](http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/STSP.html).
@@ -162,14 +165,147 @@ md" On voit que l'inégalité n'est pas respectée pour les instances `bays29`, 
 # ╔═╡ 92b578e0-2132-11eb-013e-d5292e2a9796
 md"## Algorithme HK"
 
+# ╔═╡ 9a467930-2d9c-11eb-0d2e-272a0f9bd4f6
+
+
 # ╔═╡ 96f852b0-2132-11eb-24f0-ff3b19e64821
-md"## "
+md"## Ajustement des paramètres"
+
+# ╔═╡ 7ac87e00-2d9c-11eb-3edb-49862d16f37c
+md"### Paramètres de l'algorithme RSL"
+
+# ╔═╡ 93942600-2d9c-11eb-1bbf-cb067b1b2b0a
+md"Les deux paramètres à ajuster dans l'algorithme RSL sont la méthode de recherche d'un arbre de recouvrement minimal (Kruskal ou Prim) et le sommet privilégié (la racine). Étant donné la relative rapidité de l'algorithe RSL, nous avons pu explorer les valeurs possibles de façon exhaustive pour la plupart des instances afin de trouver les meilleurs paramètres pour chacune d'entre elle.
+
+Pour les instances de taille inférieure à 100 (toutes sauf br180, gr120 et pa561) nous avons exploré les valeurs de façon exhaustive, c'est-à-dire les deux algorithmes de recouvrement Kruskal et Prim en prenant tour à tour chaque nœud comme racine.
+
+Pour les deux instances br180 et gr120, nous avons testé les deux algorithmes de recouvrement mais étant donné la lenteur de notre implémentation de la méthode Kruskal et le grand nombre de nœuds nous n'avons pas essayé de prendre chaque nœud comme racine avec la méthode Kruskal.\
+Afin de choisir judicieusement les nœuds les plus prometteurs, nous avons essayé sur les petites instances de trouver une relation entre le choix du nœud racine et la valeur de la tournée, ce qui nous aurait permis d'utiliser cette relation pour les grandes instances. Pour cela nous avons tracé le poids de la tournée obtenue en fonction de la somme et de la variance des poids des arêtes incidentes au nœud racine. Cela n'a pas porté ses fruits car on ne distingue aucune tendance sur ces tracés (voir dossier `relation_racine_resultat_rsl` à la racine si besoin).\
+Ainsi, pour les instances br180 et gr120 avec la méthode Kruskal, nous avons finalement essayé respectivement un nœud sur 10 et un nœud sur 8 (et tous les nœuds avec la méthode Prim).
+
+Pour la plus grande instance pa561, la méthode Kruskal est exclue puisqu'on n'obtient pas de résultat en temps raisonnable. Nous avons donc essayé un nœud sur 50 avec la méthode Prim.
+
+La recherche des meilleurs paramètres pour chaque instance se fait dans le fichier `rsl_parameters.jl` qui n'est pas affiché dans ce rapport. Les résultats ont été sauvegardés dans un dictionnaire (ci-dessous) contenant pour chaque instance les meilleures paramètres et le poids de la meilleure tournée obtenue."
+
+# ╔═╡ 7202e8f0-2da1-11eb-2667-0507b4c9c2c3
+md"
+`best_parameters_rsl = Dict(`\
+`\"brazil58\" => [\"prim\", 46, 28121],`\
+`\"gr17\" => [\"kruskal\", 7, 2210],`\
+`\"bayg29\" => [\"kruskal\", 17, 2014],`\
+`\"gr120\" => [\"kruskal\", 104, 8982],`\
+`\"swiss42\" => [\"kruskal\", 32, 1587],`\
+`\"brg180\" => [\"prim\", 130, 75460],`\
+`\"pa561.tsp\" => [\"prim\", 450, 3855],`\
+`\"gr21\" => [\"prim\", 13, 2968],`\
+`\"dantzig42\" => [\"prim\", 4, 890],`\
+`\"fri26\" => [\"prim\", 2, 1073],`\
+`\"hk48\" => [\"kruskal\", 20, 13939],`\
+`\"gr48\" => [\"prim\", 39, 6680],`\
+`\"gr24\" => [\"prim\", 13, 1519],`\
+`\"bays29\" => [\"kruskal\", 14, 2313])`
+"
+
+# ╔═╡ 903ee080-2d9c-11eb-22f9-194f1ce83ccf
+md"### Paramètres de l'algorithme HK"
+
+# ╔═╡ 94404480-2d9c-11eb-2d52-4bedd4fe1804
+
 
 # ╔═╡ 979e7dc0-2132-11eb-3b7f-6de3a5e098e7
-md"## "
+md"## Résultats"
 
-# ╔═╡ 9854fc80-2132-11eb-220e-1331b2d25af9
-md"## "
+# ╔═╡ 83401920-2da2-11eb-357c-8f1d3f8bf278
+md"Les résultats présentés dans cette section sont les erreurs relatives avec une tournée optimale et l'affichage des tournées obtenues quand cela est possible.
+
+**Ces résultats sont reproductibles grâce aux fichiers `main_rsl.jl` et `main_hk.jl`.**
+
+Pour cela, il faut :
+* ouvrir un REPL et se placer dans le dossier `mth6412b-starter-code`,
+* entrer la commande `include(\"projet/main_rsl.jl\")` (pour RSL, sinon `main_hk.jl`),
+* entrer la commande `main_rsl(\"gr24.tsp\")` (pour l'instance gr24)
+
+Ainsi, l'algorithme RSL va s'exécuter sur l'instance gr24 avec les meilleurs paramètres retenus dans le dictionnaires `best_parameters_rsl`.
+
+Le résultat s'affiche comme suit :"
+
+# ╔═╡ 01f2ade0-2da4-11eb-0d7c-4952127893e2
+md"
+`ALGORITHME RSL`
+
+`instance : gr24`\
+`meilleurs paramètres : prim avec comme noeud racine 13`\
+`résultat : 1519`\
+`écart relatif avec une tournée optimale : 19.42%`
+"
+
+# ╔═╡ 6adc21b0-2da4-11eb-08ab-f53337b83688
+md"Si de plus l'instance est affichable (bayg29, bays29, dantzig42, gr120), la tournée obtenue va s'afficher."
+
+# ╔═╡ 43295630-2d9d-11eb-0cd1-f9c2549fa9e8
+md"### Résultats de l'algorithme RSL"
+
+# ╔═╡ e8929140-2da7-11eb-31fd-0bca200d63c9
+md"Voici les erreurs relatives avec une tournée optimale pour chaque instance :"
+
+# ╔═╡ fb43e050-2da7-11eb-3a58-8ffac64ed1fb
+md"
+`bayg29 : 25.09%`\
+`bays29 : 14.5%`\
+`brazil58 : 10.73%`\
+`brg180 : 3769.74%`\
+`dantzig42 : 27.32%`\
+`fri26 : 14.51%`\
+`gr120 : 29.39%`\
+`gr17 : 6.0%`\
+`gr21 : 9.64%`\
+`gr24 : 19.42%`\
+`gr48 : 32.38%`\
+`hk48 : 21.62%`\
+`pa561 : 39.52%`\
+`swiss42 : 24.67%`
+"
+
+# ╔═╡ 52575470-2da9-11eb-317c-6110f116fb4d
+md"Les erreurs relatives sont comprises entre 6% et 40% - sauf sur brg180 - avec globalement une augmentation de l'erreur avec la taille du problème.
+
+L'erreur sur l'instance brg180 est extrêment elevée (3769.74%) ce qui reflète que la tournée obtenue, de poids 75460, n'a rien à voir avec la tournée optimale, de poids 1950."
+
+# ╔═╡ 627d0c12-2da8-11eb-2a81-8d5e5b4dc045
+md"Voici l'affichage des tournées obtenus pour les instances bayg29, bays29, dantzig42 et gr120."
+
+# ╔═╡ 852fa560-2da8-11eb-3311-2fff25e4ba54
+md"**bayg29**"
+
+# ╔═╡ 497dd0f0-2da8-11eb-0aa0-c7d62f5e8410
+display_img("best_tour_RSL_bayg29.png")
+
+# ╔═╡ 9333c010-2da8-11eb-285a-638a346aee85
+md"**bays29**"
+
+# ╔═╡ 57c44cd0-2d9d-11eb-3c3b-07c594781c93
+display_img("best_tour_RSL_bays29.png")
+
+# ╔═╡ 9acbb850-2da8-11eb-0164-4da7377ac6a4
+md"**dantzig42**"
+
+# ╔═╡ 450a4a30-2da8-11eb-212c-85abdfb8b1c7
+display_img("best_tour_RSL_dantzig42.png")
+
+# ╔═╡ a1bee50e-2da8-11eb-2031-099d7cc7473f
+md"**gr120**"
+
+# ╔═╡ 53a8a910-2da8-11eb-1bcd-d38b469da024
+display_img("best_tour_RSL_gr120.png")
+
+# ╔═╡ 4a519cb0-2d9d-11eb-3f6b-ad1e2dcb6a58
+md"### Résultats de l'algorithme HK"
+
+# ╔═╡ 58759b70-2d9d-11eb-1f5c-e929ff2c16ce
+
+
+# ╔═╡ 4b32ade0-2d9d-11eb-2fe1-b75b74680b94
+md"### Résultats globaux"
 
 # ╔═╡ 6ee624f0-2132-11eb-28da-8decd97e4ffc
 
@@ -180,8 +316,9 @@ md"## "
 # ╟─34337100-2132-11eb-3ec2-df46e3cc07fb
 # ╟─392bb820-2132-11eb-0682-59d3f3bc732c
 # ╠═6a686500-2132-11eb-0a1a-6d0bc5b501a9
+# ╠═284f3500-2da7-11eb-01bb-8b18721a7ff8
 # ╟─8a7ab0ee-2132-11eb-27b6-e14b6729fc3b
-# ╟─b1f6e040-2132-11eb-1c11-23ed2e7b6541
+# ╠═b1f6e040-2132-11eb-1c11-23ed2e7b6541
 # ╟─1277a8e0-2134-11eb-039a-5ff5e9101f4a
 # ╟─a7a6775e-2135-11eb-15ec-61a5327c4948
 # ╟─0f27cfb0-2136-11eb-0dd8-09e166004a49
@@ -213,7 +350,31 @@ md"## "
 # ╟─084df880-2150-11eb-0811-9365c0f2147c
 # ╟─31516ea0-2151-11eb-2fec-6122b537a0fa
 # ╟─92b578e0-2132-11eb-013e-d5292e2a9796
-# ╠═96f852b0-2132-11eb-24f0-ff3b19e64821
-# ╠═979e7dc0-2132-11eb-3b7f-6de3a5e098e7
-# ╠═9854fc80-2132-11eb-220e-1331b2d25af9
+# ╠═9a467930-2d9c-11eb-0d2e-272a0f9bd4f6
+# ╟─96f852b0-2132-11eb-24f0-ff3b19e64821
+# ╟─7ac87e00-2d9c-11eb-3edb-49862d16f37c
+# ╟─93942600-2d9c-11eb-1bbf-cb067b1b2b0a
+# ╟─7202e8f0-2da1-11eb-2667-0507b4c9c2c3
+# ╟─903ee080-2d9c-11eb-22f9-194f1ce83ccf
+# ╠═94404480-2d9c-11eb-2d52-4bedd4fe1804
+# ╟─979e7dc0-2132-11eb-3b7f-6de3a5e098e7
+# ╟─83401920-2da2-11eb-357c-8f1d3f8bf278
+# ╟─01f2ade0-2da4-11eb-0d7c-4952127893e2
+# ╟─6adc21b0-2da4-11eb-08ab-f53337b83688
+# ╟─43295630-2d9d-11eb-0cd1-f9c2549fa9e8
+# ╟─e8929140-2da7-11eb-31fd-0bca200d63c9
+# ╟─fb43e050-2da7-11eb-3a58-8ffac64ed1fb
+# ╟─52575470-2da9-11eb-317c-6110f116fb4d
+# ╟─627d0c12-2da8-11eb-2a81-8d5e5b4dc045
+# ╟─852fa560-2da8-11eb-3311-2fff25e4ba54
+# ╟─497dd0f0-2da8-11eb-0aa0-c7d62f5e8410
+# ╟─9333c010-2da8-11eb-285a-638a346aee85
+# ╟─57c44cd0-2d9d-11eb-3c3b-07c594781c93
+# ╟─9acbb850-2da8-11eb-0164-4da7377ac6a4
+# ╟─450a4a30-2da8-11eb-212c-85abdfb8b1c7
+# ╟─a1bee50e-2da8-11eb-2031-099d7cc7473f
+# ╟─53a8a910-2da8-11eb-1bcd-d38b469da024
+# ╟─4a519cb0-2d9d-11eb-3f6b-ad1e2dcb6a58
+# ╠═58759b70-2d9d-11eb-1f5c-e929ff2c16ce
+# ╟─4b32ade0-2d9d-11eb-2fe1-b75b74680b94
 # ╠═6ee624f0-2132-11eb-28da-8decd97e4ffc
